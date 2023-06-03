7623d9204dfde0a82ef726f43186d85656ea3675
Aparajit-Garg <aparajit.garg06@gmail.com>
2023-06-04 01:52:33 +0530

    header done

--- clone/src/pages/Landing/Landing.jsx
+++ clone/src/pages/Landing/Landing.jsx
@@ -1,15 +1,27 @@
-import {useState} from 'react';
+// import {useState} from 'react';
 import classes from "./Landing.module.css";
-import Header from "../../components/Header/Header";
+// import Header from "../../components/Header/Header";
 
 const LandingPage = () => {
 
     return (
         <div>
-            <div className={classes.image}>
-                <img src="https://b.zmtcdn.com/web_assets/81f3ff974d82520780078ba1cfbd453a1583259680.png" alt="header image" />
+            <div className={classes.container}>
+                <img src="https://b.zmtcdn.com/web_assets/81f3ff974d82520780078ba1cfbd453a1583259680.png" alt="zomato blah" />
+                <div className={classes.overlay}>
+                    <ul>
+                        <li>
+                            Add a restaurant
+                        </li>
+                        <li>
+                            Log In
+                        </li>
+                        <li>
+                            Sign Up
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <Header />
         </div>
     )
 }
